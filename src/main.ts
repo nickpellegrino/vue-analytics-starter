1a2a38be3f7370eb7f0951343b652dd8a0646ad1
Nicolas Pellegrino <nick@nickp.com>
2025-09-12 22:04:15 -0400

    add ts file and remove old

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+// Track Vue Router navigation
+router.afterEach((to) => {
+  window.dataLayer = window.dataLayer || []
+  window.dataLayer.push({
+    event: 'pageview',
+    page_path: to.fullPath,
+    page_title: document.title,
+  })
+})
 
+app.use(router)
 app.mount('#app')
